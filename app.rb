657848798ab1acad31855dfaba72ad179a34db4a
Luke Chen <luke.chen.2010@gmail.com>
2020-05-28 18:21:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,12 +5,12 @@ class App < Sinatra::Base
   
   get '/reversename/:name' do
     name = params[:name]
-    name.reverse
+    return name.reverse
   end
 
   get '/square/:number' do
     number = params[:number].to_i
-    (number**2).to_s
+    return (number**2).to_s
   end
   
   get '/say/:number/:phrase' do
@@ -24,7 +24,7 @@ class App < Sinatra::Base
     end
     
     output = output + "."
-    output
+    return output
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
