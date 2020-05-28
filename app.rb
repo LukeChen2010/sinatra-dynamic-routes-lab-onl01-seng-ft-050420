3456bbecedf0a7caec6a9d06582fad37ef9bb268
Luke Chen <luke.chen.2010@gmail.com>
2020-05-28 18:20:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,11 +19,11 @@ class App < Sinatra::Base
     output = ""
     
     while number > 0
-      output << phrase
+      output += phrase
       number -= 1
     end
     
-    output << "."
+    output += "."
     output
   end
   
